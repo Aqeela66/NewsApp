9bb18771bfb3069536054756c9cab40c4296037d
Aqeela66 <83452367+Aqeela66@users.noreply.github.com>
2021-08-16 21:49:30 +0500

    News App implemented by fatching the API of news app

---
+++ app.js
@@ -0,0 +1,28 @@
+fetch('https://newsapi.org/v2/everything?q=tesla&apiKey=de76ddd2976543a2ac1de4afec60bba8').then(function(response) {
+        console.log(response, 'Response');
+        return response.json();
+    }).then(newsData)
+    .catch(function(err) {
+        console.log(err);
+    });;
+
+function newsData(obj) {
+    let newsArray = obj.articles;
+    console.log(newsArray);
+    console.log(typeof newsArray);
+    for (news in newsArray)
+        console.log(newsArray[news])
+
+    document.getElementById("main-container").innerHTML = (newsArray.map(news =>
+        `
+    
+      <div class="main">
+      <div class="content">
+      <img  src="${news.urlToImage}" />
+        <button id = "btnMore" onclick= "getElementById('moreInfo').innerHTML='author: ${news.author}<br> news title: ${news.title}<br> news description ${news.description}  '" >  More Details </button>
+     </div> 
+    </div>`
+
+        // newsArray.innerHTML = newsHtml;
+    ).join(''));
+}
