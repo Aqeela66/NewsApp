fd94d463a7b165348a3fd254cc673b91e76ab24a
Aqeela <83452367+Aqeela66@users.noreply.github.com>
2021-09-11 04:17:35 +0500

    News App build through API

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
-fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-08-10&sortBy=publishedAt&apiKey=de76ddd2976543a2ac1de4afec60bba8').then(function(response) {
-        console.log(response, 'Response');
-        return response.json();
-    }).then(newsData)
-    .catch(function(err) {
-        console.log(err);
-    });;
+console.log("This is my index js file");
 
-function newsData(obj) {
-    let newsArray = obj.articles;
-    console.log(newsArray);
-    console.log(typeof newsArray);
-    for (news in newsArray)
-        console.log(newsArray[news])
+// Initialize the news api parameters
+let source = 'the-times-of-india';
+let apiKey = 'd093053d72bc40248998159804e0e67d'
 
-    document.getElementById("main-container").innerHTML = (newsArray.map(news =>
-        `
-    
-      <div class="main">
-      <div class="content">
-      <img  src="${news.urlToImage}" />
-        <button id = "btnMore" onclick= "getElementById('moreInfo').innerHTML='author: ${news.author}<br> news title: ${news.title}<br> news description ${news.description}  '" >  More Details </button>
-     </div> 
-    </div>`
+// Grab the news container
+let newsAccordion = document.getElementById('newsAccordion');
 
-        // newsArray.innerHTML = newsHtml;
-    ).join(''));
+// Create an ajax get request
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`, true);
+
+// What to do when response is ready
+xhr.onload = function () {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles;
+        console.log(articles);
+        let newsHtml = "";
+        articles.forEach(function(element, index) {
+            // console.log(element, index)
+            let news = `<div class="card">
+                            <div class="card-header" id="heading${index}">
+                                <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                    aria-expanded="false" aria-controls="collapse${index}">
+                                   <b>Breaking News ${index+1}:</b> ${element["title"]}
+                                </button>
+                                </h2>
+                            </div>
+
+                            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
+                                <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
+                            </div>
+                        </div>`;
+            newsHtml += news;
+        });
+        newsAccordion.innerHTML = newsHtml;
+    }
+    else {
+        console.log("Some error occured")
+    }
 }
+
+xhr.send()
